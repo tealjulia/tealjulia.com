f093d93ed60ece70d4d930e55a9fe92dcc95981b
Teal Larson <larson.teal@gmail.com>
2021-02-03 14:14:44 -0800

    skills + colors update

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function navToggle() {
         nav.classList.toggle('flex');
         nav.classList.toggle('hidden');
     }
+
+
+  
+
+    var acc = document.getElementsByClassName('accordion');
+    var i;
+    console.log(document.getElementsByClassName("accordion").length)
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+
